4acbece81d5fbb51fea5f83afc95b3bc4b991835
warzes <warzes@mail.ru>
2024-04-16 10:52:32 +1000

    - создание проекта

---
+++ src/3rdparty/3rdparty.cpp
@@ -0,0 +1,3 @@
+﻿void fnMy3rdparty()
+{
+}
---
+++ src/Game/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "stdafx.h"
+
+//-----------------------------------------------------------------------------
+#if defined(_MSC_VER)
+#	pragma comment( lib, "3rdparty.lib" )
+#	pragma comment( lib, "Engine.lib" )
+#endif
+
+int main()
+{
+}
